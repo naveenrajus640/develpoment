1e5dea060d5e145478b5ab1d844aa75b80affd72
naveenrajus640 <158122820+naveenrajus640@users.noreply.github.com>
2024-02-10 23:28:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch33 team Raju");
-      System.out.println("this is the pipine job created to automatically start changes it be complied and run");
+      System.out.println("this is the pipeline job created to automatically start changes it be complied and run");
     }  
 } 
